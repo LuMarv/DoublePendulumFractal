6bd3bb3845a1b2f5a24b8539bd19f4affdee620d
Marvin <vu252@stud.uni-heidelberg.de>
2026-02-03 13:58:46 +0100

    modified:   CMakeLists.txt 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,5 @@
-#include "src/Simulation.hpp"
-#include "src/IntegratorEuler.hpp"
-#include "src/State.hpp"
+#include <iostream>
 
 int main() {
-#ifdef USE_EULER
-    run_simulation<IntegratorEuler>(10, 10, 100, 0.01);
-#else
-#error "No integrator selected! Define USE_EULER or USE_RK4."
-#endif
-  DoublePendulum<IntegratorEuler> p;
-  p.printState();
-  // test
-  State s1(1.0, 2.0, 3.0, 4.0);
-  State s2(1.0, 2.0, 3.0, 4.0);
-  s1.print();
-  s2.print();
-  (s1 + s2).print();
+  std::cout << "Hello this is the first test (for CMakeLists.txt)" << '\n';
 }
