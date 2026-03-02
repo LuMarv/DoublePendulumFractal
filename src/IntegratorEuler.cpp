43f66acee8faf0b1bf19becd332293d92ab52b34
JanVStraub <janvastraub@gmail.com>
2026-03-02 19:08:00 +0100

    slight additions to Euler, new error message

--- src/IntegratorEuler.cpp
+++ src/IntegratorEuler.cpp
@@ -15,7 +15,7 @@ void IntegratorEuler::integrate(const double t0,
 
   try {
     double integration_time = t1-t0;
-    if(integration_time>0) {
+    if(integration_time > 0 && dt > 0) {
 
       double time = t0;
       double eval_counter = 0;
@@ -23,7 +23,7 @@ void IntegratorEuler::integrate(const double t0,
       // to save first state
       if(eval_counter < t_eval.size() && time <= t_eval[eval_counter]) {
         // or interpolate
-        eval_states.push_back(y);
+        eval_states[eval_counter] = y;
         eval_counter++;
       }
 
@@ -32,18 +32,23 @@ void IntegratorEuler::integrate(const double t0,
         
         if(eval_counter < t_eval.size() && time <= t_eval[eval_counter]) {
           // or interpolate
-          eval_states.push_back(y);
+          eval_states[eval_counter] = y;
           eval_counter++;
         }
         time += dt;
       }
     }
     else {
-      throw(integration_time);
+      throw(integration_time, dt);
     }
   }
-  catch (double time){
-    std::cerr << "Error occured: The integration time is " << time << ", but should be larger than zero.";
+  catch (double time, double dt){
+    if(time <0){
+      std::cerr << "Error occured: The integration time is " << time << ", but should be larger than zero.";
+    }
+    if(dt < 0){
+      std::cerr << "Error occured: The time-integration interval is " << dt << ", but should be larger than zero.";
+    }
   }
   
 
