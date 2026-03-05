d1732564e3adcca0e22981668066b22bcc1ab6ac
Marvin <vu252@stud.uni-heidelberg.de>
2026-03-05 17:03:35 +0100

    Formatted simulation.cpp

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -2,37 +2,37 @@
 
 void simulate(const int integrator_index, const double time_end, const double step_size) {
 
-    int size1 = 1; // pixels x
-    int size2 = 1; // pixels y
-    // int size3 = int(time_end / step_size) + 1; // #integration steps
-    int size3 = 2000; // #integration steps
-    int size4 = time_end; // integration time
+  int size1 = 1; // pixels x
+  int size2 = 1; // pixels y
+  // int size3 = int(time_end / step_size) + 1; // #integration steps
+  int size3 = 2000; // #integration steps
+  int size4 = time_end; // integration time
 
-    std::vector<std::vector<std::vector<State>>> t(
-        size1, std::vector<std::vector<State>>(
-        size2, std::vector<State>(
-        size3)));
+  std::vector<std::vector<std::vector<State>>> t(
+    size1, std::vector<std::vector<State>>(
+    size2, std::vector<State>(
+    size3)));
 
-    for(int i = 0; i < size1; i++){
-        for(int j = 0; j < size2; j++){
+  for(int i = 0; i < size1; i++){
+    for(int j = 0; j < size2; j++){
             
-            // how to decide on integrator type? 
-            // -> if statements are no bottleneck whatsoever here
-            std::unique_ptr<Integrator> I;
+      // how to decide on integrator type? 
+      // -> if statements are no bottleneck whatsoever here
+      std::unique_ptr<Integrator> I;
 
-            switch(integrator_index){
-                case 0:
-                    I = std::make_unique<IntegratorEuler>();
-                    break;
-                case 1:
-                    //I = std::make_unique<IntegratorRK4>();
-                    break;
-                default:
-                    I = std::make_unique<IntegratorEuler>();
-            }
+      switch(integrator_index){
+        case 0:
+          I = std::make_unique<IntegratorEuler>();
+          break;
+        case 1:
+          //I = std::make_unique<IntegratorRK4>();
+          break;
+        default:
+          I = std::make_unique<IntegratorEuler>();
+      }
 
-            Pendulum p(t[0][0], std::move(I)); // rule of five
-            p.run(); // has to be implemented
-        }
+      Pendulum p(t[i][j], std::move(I)); // rule of five
+      p.run(); // has to be implemented
     }
+  }
 }
